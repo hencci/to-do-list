d7fa9cd31b01e10fcba691d7235d1feb3e2b21f6
hencci <hencci20@gmail.com>
2025-04-04 00:13:51 +0100

    create function to add project on click event

---
+++ src/modules/app.js
@@ -0,0 +1,21 @@
+import { Project } from './project';
+import { Todo } from './todo';
+import { saveProjects, loadProjects } from './storage';
+import { renderProjects } from './ui';
+
+let projects = loadProjects() || [Project("Default")];
+
+const initApp = () => {
+    renderProjects();
+    
+    document.getElementById("addProjectBtn").addEventListener("click", () => {
+        const projectName = prompt("Enter project name:");
+        if (projectName) {
+            projects.push(Project(projectName));
+            saveProjects(projects);
+            renderProjects();
+        }
+    });
+};
+
+export { initApp };
