95bcd93ec659644fea7615965196e86e525461ed
hencci <hencci20@gmail.com>
2025-04-03 13:53:01 +0100

    Implement project

---
+++ src/modules/project.js
@@ -0,0 +1,16 @@
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.todos = [];
+    }
+
+    addTodo(todo) {
+        this.todos.push(todo);
+    }
+
+    removeTodo(todoTitle) {
+        this.todos = this.todos.filter(todo => todo.title !== todoTitle);
+    }
+}
+
+export { Project };
