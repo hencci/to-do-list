2fb019247bed8bf6c8e68f20ec05faf3cf15a5be
hencci <hencci20@gmail.com>
2025-04-06 13:31:27 +0100

    Manage current project and rendering

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -2,29 +2,48 @@ import { Project } from './project';
 import { Todo } from './todo';
 import { saveProjects, loadProjects } from './storage';
 
-const projects = loadProjects() || [Project("Default")];
+let projects = [];
+let currentProject = null;
+
+const setProjects = (loadedProjects) => {
+    projects = loadedProjects;
+    if (!currentProject) currentProject = projects[0];
+};
 
 const renderProjects = () => {
     const projectContainer = document.getElementById("projects");
     projectContainer.innerHTML = "";
-    
+
     projects.forEach((project) => {
         const projectElement = document.createElement("div");
         projectElement.textContent = project.name;
-        projectElement.addEventListener("click", () => renderTodos(project));
+        projectElement.style.cursor = "pointer";
+
+        projectElement.addEventListener("click", () => {
+            currentProject = project;
+            renderTodos();
+        });
+
         projectContainer.appendChild(projectElement);
     });
 };
 
-const renderTodos = (project) => {
+const renderTodos = () => {
     const todoContainer = document.getElementById("todos");
     todoContainer.innerHTML = "";
 
-    project.todos.forEach((todo) => {
+    currentProject.todos.forEach((todo) => {
         const todoElement = document.createElement("div");
-        todoElement.textContent = `${todo.title} - ${todo.dueDate}`;
+        todoElement.classList.add("todo");
+        todoElement.textContent = `${todo.title} - ${todo.dueDate} (${todo.priority})`;
         todoContainer.appendChild(todoElement);
     });
 };
 
-export { renderProjects, renderTodos };
+const addTodoToCurrentProject = (todo) => {
+    currentProject.todos.push(todo);
+    saveProjects(projects);
+    renderTodos();
+};
+
+export { setProjects, renderProjects, renderTodos, addTodoToCurrentProject };
